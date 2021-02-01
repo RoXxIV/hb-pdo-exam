a860ed64cfa148fc660bd6df33b381bf2e4a2acf
RoXxIV <evan.hermier@gmail.com>
2021-02-01 13:10:38 +0100

    1. Se connecter et utiliser la base de données

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -1,7 +1,13 @@
 <?php
 
-$dbName = 'beanies';
+$dbName = 'beanies_exam';
 $dbUser = 'root';
 $dbPass = ''; // root si vous utilisez un Mac avec MAMP
 $dbHost = 'localhost'; // ou 127.0.0.1, en cas de soucis (mais les deux devraient être identiques)
 $dbDsn = 'mysql:dbname='.$dbName.';host='.$dbHost;
+
+try {
+    $connection = new PDO($dbDsn, $dbUser, $dbPass);
+} catch (PDOException $e) {
+    echo 'Connexion échouée : ' . $e->getMessage();
+}
